39e8a4ecd417fbb346c8581c58a59a6996593227
Mohammad Dehghanpour <m.dehghanpour10@gmail.com>
2021-05-25 00:34:38 +0430

    main chat component added

--- components/chat.vue
+++ components/chat.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="wrapper">
-    <div class="chat"></div>
+    <div class="chat_wrapper">
+      <div class="chat"></div>
+
+    </div>
 
   </div>
 
@@ -18,11 +21,26 @@
     height: 100%;
     position: fixed;
     padding: 100px 0 70px 0;
+    background-color: rgba(0,0,0,0);
+  }
+  .chat_wrapper{
+    width: 100%;
+    height: 100%;
+    background-image: url("/picture/hero-bg.jpg");
+    background-size: 100% 100%;
+    position: relative;
+    border-radius: 10px;
+
   }
   .chat{
     width: 100%;
     height: 100%;
-    background-color: blue;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rgba(0,0,0,0.4);;
+    border-radius: 10px;
+
   }
   @media only screen and (max-width: 900px) {
     .wrapper {
